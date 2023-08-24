1e9f83272f8142d4934bd9491c6d2abe8077cc2d
Florian <florian.delobelle@gmail.com>
2023-08-24 18:47:51 +0200

    created signin page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router'
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
 
-     const navigate = useNavigate()
+     const navigate = useNavigate();
 
-    const location = useLocation()
-    console.log(location.pathname)
+    const location = useLocation();
+    
+    console.log(location.pathname);
 
     function pathMatchRoute(route){
         if(route === location.pathname){
-            return true
-        }
+            return true;
+        };
     }
 
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
     <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
     <div>
 <img src="/logo.png" alt="logo" className="h-6 cursor-pointer" onClick={() => navigate("/")}/>
@@ -23,19 +24,16 @@ export default function Header() {
     <div>
     <ul className="flex space-x-6">
         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-            pathMatchRoute("/") && "text-black border-b-red-500"
-        }`}
+            pathMatchRoute("/") && "text-black border-b-red-400"}`}
         onClick={() => navigate("/")}
         >Accueil</li>
         {/* [3px] = custom tailwind size */}
         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-            pathMatchRoute("/annonces") && "text-black border-b-red-500"
-        }`}
+            pathMatchRoute("/annonces") && "text-black border-b-red-400"}`}
         onClick={() => navigate("/annonces")}
         >Annonces</li>
         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-            pathMatchRoute("/se-connecter") && "text-black border-b-red-500"
-        }`}
+            pathMatchRoute("/se-connecter") && "text-black border-b-red-400"}`}
         onClick={() => navigate("/se-connecter")}
         >Se connecter</li>
     </ul>

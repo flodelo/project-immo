c8899e1fb5225367c3ccc4f6428b16f144c46a04
Florian <florian.delobelle@gmail.com>
2023-08-24 12:52:14 +0200

    create pages and routes with react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <>
-    <h1 className="text-2xl">Test</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/profil" element={<Profile/>} />
+        <Route path="/se-connecter" element={<SignIn/>} />
+        <Route path="/creer-compte" element={<SignUp/>} />
+        <Route path="/oubli-mdp" element={<ForgotPassword/>} />
+        <Route path="/annonces" element={<Offers/>} />
+      </Routes>
+    </Router>
     </>
   );
 }
